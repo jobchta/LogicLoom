b5bb3e857ab6a9df6922a0df852f2bf0093b8e78
jobchta <byte4bits4@gmail.com>
2026-01-06 14:40:35 +0530

    Initialize animationRef with zero

--- components/effects/ParticleBackground.tsx
+++ components/effects/ParticleBackground.tsx
@@ -16,7 +16,7 @@ export default function ParticleBackground() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const particlesRef = useRef<Particle[]>([]);
     const mouseRef = useRef({ x: 0, y: 0 });
-    const animationRef = useRef<number>();
+    const animationRef = useRef<number>0();
 
     useEffect(() => {
         const canvas = canvasRef.current;
